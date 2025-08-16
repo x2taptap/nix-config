0efea2683d1c21654e63372ab3340b06899b0eaa
x2taptap <kasjan887@gmail.com>
2025-08-16 06:55:11 +0200

    Edid

--- configuration.nix
+++ configuration.nix
@@ -75,12 +75,8 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -93,7 +89,6 @@
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       kdePackages.kate
-    #  thunderbird
     ];
   };
 
@@ -113,22 +108,22 @@
      vim
      wget
      git
+     gh
      fastfetch
      vscode
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -136,13 +131,7 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "25.05"; 
   # Enable OpenGL
   hardware.graphics = {
     enable = true;
@@ -181,4 +170,13 @@
 	intelBusId = "PCI:0:02:0";
 	nvidiaBusId = "PCI:01:0:0";
   };
+  hardware.display.edid.packages = [
+    (pkgs.runCommand "edid-custom" { } ''
+      mkdir -p $out/lib/firmware/edid
+      base64 -d > "$out/lib/firmware/edid/custom1.bin" <<'EOF'
+      AP///////wAGELWcAAAAABwTAQSlPCJ4Im+xp1VMniUMUFQAAAABAQEBAQEBAQEBAQEBAQEBNGgAoKCgLlAwIDUAALAxAAACGh0AgFHQHCBAgDUAVVAhAAAcAAAAAAAAAAAAAAAAAAAAAAAAAAAA/ABDb2xvciBMQ0QKICAgAT0CAwzBIwkHB4MBAABWXgCgoKApUDAgNQBVUCEAABoaHQCAUdAcIECANQBVUCEAABwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEw==
+      EOF
+    '')
+  ];
+  hardware.display.outputs."DP-1".edid = "custom1.bin";
 }
