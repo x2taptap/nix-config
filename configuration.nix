cefaac719bc9e08fb4d7f7f54cdc86a8d89001ae
x2taptap <kasjan887@gmail.com>
2025-08-23 14:19:09 +0200

    gnome

--- configuration.nix
+++ configuration.nix
@@ -52,8 +52,8 @@
   };
   services.xserver.enable = true;
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
   # Enable automatic login for the user.
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "user";
